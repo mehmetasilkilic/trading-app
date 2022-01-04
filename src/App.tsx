b8a87eaefc6ba4204dc693dc0b69e3729f1ff960
mehmetasilkilic <mehmetasil96@gmail.com>
2022-01-04 10:15:25 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./assets/scss/main.scss"
+import { useState } from "react";
+import { createTheme } from "@mui/material"
 
-function App() {
+const App: React.FC = () => {
+
+  const [themeMode, setThemeMode] = useState<"light" | "dark">("dark");
+
+  const theme = createTheme({
+    palette: {
+      mode: themeMode,
+    },
+    typography: {
+      fontSize: 14,
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      a
     </div>
   );
 }
